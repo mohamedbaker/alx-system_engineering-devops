365b0a0865d5d5f634abe30e3544820db61075c5
mohamedbaker <dam.vampire22@gmail.com>
2024-04-18 23:48:47 +0200

    increase file limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Fix problem of high amount files opened
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
